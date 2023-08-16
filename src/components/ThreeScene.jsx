e691368c4130235a456c016708d2f2b38a69a547
MoldyLunchBox <duu116@gmail.com>
2023-08-16 01:18:23 +0100

    switched to disk shape, the displacement map look isnt the best

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -9,28 +9,81 @@ const ThreeScene = () => {
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
-
+        camera.position.set(0, 1, 0);
         renderer.setSize(window.innerWidth, window.innerHeight);
         sceneRef.current.appendChild(renderer.domElement);
 
 
         // texture loading
         const textureLoader = new THREE.TextureLoader();
-        const texture = textureLoader.load('/path/to/your/texture.jpg');
+        const texture = textureLoader.load('/textures/earthmap.jpeg');
+        const displacementMap = textureLoader.load('/textures/earthbump.jpeg');
+        const circleGeometry = new THREE.CircleGeometry(1, 32);
+        // const earthMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00, roughness: 0.5, metalness: 0.7 });
+        // const earthDisk = new THREE.Mesh(circleGeometry, material);
+
+        const earthGeometry = new THREE.RingGeometry(0, 2, 32);
+        const earthMaterial = new THREE.MeshStandardMaterial({  map: texture,  displacementMap: displacementMap,
+            displacementScale: 0.5, // Adjust the scale of the displacement effect
+            roughness: 0.5,
+            metalness: 0.7 });
+
+            // const displacementMap = textureLoader.load('/textures/earthbump.jpeg');
+            // const geometry = new THREE.RingGeometry(0, 2, 32);
+            // const material = new THREE.MeshStandardMaterial({  map: texture,  displacementMap: displacementMap,
+            //     displacementScale: 0.5, // Adjust the scale of the displacement effect
+            //     roughness: 0.5,
+            //     metalness: 0.7,side: THREE.DoubleSide });
+            // const ringMesh = new THREE.Mesh(geometry, material)
+            // ringMesh.rotation.x = Math.PI / 2;
+            // scene.add(ringMesh);
+     
+            // Create a 3D disk with thickness using ExtrudeGeometry
+    const shape = new THREE.Shape();
+    shape.absarc(0, 0, 1, 0, Math.PI * 2);
     
-        const geometry = new THREE.RingGeometry(0, 2, 32);
-        const material = new THREE.MeshStandardMaterial({  map: texture, side: THREE.DoubleSide });
-        const ringMesh = new THREE.Mesh(geometry, material)
-        scene.add(ringMesh);
+    const extrudeSettings = {
+      steps: 1,
+      depth: 0.2, // Adjust the thickness
+      bevelEnabled: false,
+    };
+    
+    // const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    // const material = new  THREE.MeshStandardMaterial({  map: texture,  displacementMap: displacementMap,   displacementScale: 0.5, // Adjust the scale of the displacement effect
+    // roughness: 0.5,
+    // metalness: 0.7 });
+    // const disk = new THREE.Mesh(geometry, material);
+    // disk.rotation.x = -Math.PI / 2;
+    const cylinderGeometry = new THREE.CylinderGeometry(1, 1, 0.2, 32);
+    const colors = [
+        new THREE.Color('#000000'), // Material 0
+        new THREE.Color('#000000'), // Material 1
+        new THREE.Color('#000000'), // Material 5
+      ];
+    const material = new THREE.MeshStandardMaterial({ map: texture,  displacementMap: displacementMap,   displacementScale: 0.5, // Adjust the scale of the displacement effect
+    roughness: 0.5,
+    metalness: 0.7 });
+
+    const material1 = new THREE.MeshBasicMaterial({ attach:"material-0", color:"#ff0000" })
+    const material2 = new THREE.MeshStandardMaterial({ attach:"material-1",   map: texture,  displacementMap: displacementMap,   displacementScale: 1, // Adjust the scale of the displacement effect
+    roughness: 0.5,
+    metalness: 0.7 });  
+    const material3 = new THREE.MeshBasicMaterial({ attach:"material-2", color:"#00ff00" })
+    const materials = [material1, material2, material3];
+    const cylinder = new THREE.Mesh(cylinderGeometry, materials);
+
+    // cylinder.rotation.x = Math.PI / 2;
+    scene.add(cylinder);
+
+    // scene.add(cylinder);
+
 
         camera.position.z = 5;
 
         const animate = () => {
             requestAnimationFrame(animate);
-
-            ringMesh.rotation.x += 0.01
-            ringMesh.rotation.y += 0.01;
-
+            cylinder.rotation.x += 0.01
+            cylinder.rotation.y += 0.01;
             renderer.render(scene, camera);
         };
 
