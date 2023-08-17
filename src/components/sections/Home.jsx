07e06e31cc90895a42ea9552d92e1fa292e9eb9e
MoldyLunchBox <duu116@gmail.com>
2023-08-17 20:45:11 +0100

    hm yesterdays commit didnt go thru?

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,9 +3,9 @@ import ThreeScene from '../ThreeScene'
 
 export const Home = () => {
     return (
-        <div className='bg-white flex w-full  h-[100vh]  '>
-            
-            <div className='w-full text-white'> one</div>
+        <div className=' flex w-full bg-white h-[100vh]  '>
+
+            <div className='w-full h-1/2 bg-yellow-300 text-white'> one</div>
             <div id="threejs" className='bg-white w-full '>
  two
                 {/* <ThreeScene /> */}
