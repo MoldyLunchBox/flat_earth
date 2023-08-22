ff7c61b1a5bb5ecccacb36c7518ef88118a10249
MoldyLunchBox <duu116@gmail.com>
2023-08-22 01:11:56 +0100

    particles

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import { Navbar } from './components/Navbar';
 import { GetInvolved } from './components/sections/GetInvolved';
 function App() {
   return (
-    
-    <div  className=" flex overflow-hidden flex-col w-full h-full  ">
-      <div className='h-screen z-1 relative flex flex-col bg-gradient-to-t from-[#1e293b]  to-black'>
-       
+
+    <div className=" flex overflow-hidden  flex-col w-full h-full  ">
+      <section className='h-screen relative flex flex-col bg-gradient-to-t from-[#1e293b]  to-black'>
         <Home />
-      <div className='curve'></div>
-      </div>
-      <div className='h-screen -z-10 bg-[#166534] flex flex-col'>
+        <div className='curve'></div>
+      </section>
+
+      <section className='h-screen relative -z-10 bg-[#1e3a8a] flex flex-col justify-center'>
         <GetInvolved />
-        </div>
+        <div className='bubble justify-end '></div>
+
+      </section>
+
+      <section className='h-screen dark   flex flex-col'>
+        <GetInvolved />
+      </section>
     </div>
   );
 }
--- src/components/sections/GetInvolved.jsx
+++ src/components/sections/GetInvolved.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-
+import React, { useCallback } from 'react'
+import { ParticlesContainer } from '../ParticlesContainer'
 export const GetInvolved = () => {
+
+
     return (
-            <div className='flex  w-full flex-row h-full items-center space-x-4 '>
 
-                <div className='flex space-y-4 flex-col w-full h-1/2  justify-center  text-white '>
-                    <div className='text-3xl font-semibold bg-gradient-to-r text-transparent bg-clip-text from-[#4d7c0f] to-[#777777]'>Where's the Edge?</div>
-
-                    <div className='text-xl font-poppings '>Lost at the edge of the world?  where the world's a disc and laughter echoes endlessl? Don't worry, Join us at the horizon of hilarity, where laughter knows no curvature.</div>
-                    <button className="ml-auto rounded-lg px-4 py-2 text-lg  bg-gradient-to-r from-black to-gray-800  w-fit">Join us </button >
-                </div>
-                <div id="threejs" className='h-1/2   text-white w-full '>
+        <div className='flex max-w-[1000px] mx-auto flex-row h-full items-center space-x-4'>
+        <div className="flex relative flex-col justify-center p-8">
+                <ParticlesContainer />
+                <h2 className="text-4xl font-bold text-white mb-4">Protecting Our Earth</h2>
+                <p className="text-white">
+                    Let's join hands to preserve the beauty of our planet. Every small step we take matters,
+                    from recycling to reducing our carbon footprint. Together, we can make a difference.
+                </p>
+                <button className="ml-auto rounded-lg text-white px-4 py-2 text-lg  bg-gradient-to-r from-black to-gray-800  w-fit">Join us </button >
+            </div>
+            <div className="card  bg-base-100 w-full  shadow-xl">
+                <figure><img src="/images/tinyTree.jpg" alt="Shoes" /></figure>
+                <div className="card-body bg-white">
+                    <h2 className="card-title">
+                        Shoes!
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
                 </div>
             </div>
+        </div>
     )
 }
