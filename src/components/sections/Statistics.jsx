2e7468e842a8f07f8027eb08ea4e02dddc7329fd
MoldyLunchBox <duu116@gmail.com>
2023-08-22 04:16:24 +0100

    update

--- src/components/sections/Statistics.jsx
+++ src/components/sections/Statistics.jsx
@@ -11,6 +11,8 @@ export const Statistics = () => {
                         <img src="/images/dry.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                         <div>
                             <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                            <Stats />
                             <button className="btn btn-primary">Get Started</button>
